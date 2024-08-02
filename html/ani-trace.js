839453905b6a049f7e996e9a384f2dba822fb550
itorr-i7 <592668024@qq.com>
2024-08-03 05:22:12 +0800

    hw

---
+++ html/ani-trace.js
@@ -0,0 +1,251 @@
+const randNamesLength = 4;
+const defaultScound = 60;
+
+const getUnix = () => +new Date();
+
+class Round {
+	constructor({ names, answer, image, imageEl, created }){
+		this.names = names;
+		this.answer = answer;
+		this.image = image;
+		// this.imageEl = imageEl;
+		this.created = created;
+		this.value = null;
+	}
+	get name(){
+		return this.names[this.answer];
+	}
+	get isRight(){
+		return this.value === this.answer;
+	}
+
+}
+class Game {
+	constructor(){
+		this.status = 'wait'; // wait, playing, end
+		this.bangumis = [];
+		this.startUnix = null;
+		this.nowUnix = null;
+		this.rounds = [];
+		this.plusScound = 0;
+		this.leftScound = null; // 剩余时间
+		this.timer = 0;
+		this.score = 0;
+		this.nowRoundIndex = 0;
+		this.progress = 0;
+		this.createRounds(5);
+		
+		this.timer = setTimeout(() => {
+			this.startGame();
+		}, 3000);
+	}
+	get allRounds(){
+		return this.rounds.slice(0,this.nowRoundIndex);
+	}
+	get Bangumis(){
+		const { bangumis } = this;
+		const Bangumis = {};
+		for(const bangumi of bangumis){
+			Bangumis[bangumi.name] = bangumi;
+		}
+	}
+	get nowRound(){
+		return this.rounds[this.nowRoundIndex];
+	}
+	get totalTime(){
+		return Math.round(( this.endUnix - this.startUnix ) / 100) / 10;
+	}
+	startGame(){
+		this.startUnix = getUnix();
+		this.plusScound = 0; // 清空加时
+		this.status = 'playing';
+		this.nowRoundIndex = 0;
+		this.step();
+	}
+	createRounds(length){
+		const rounds = new Array(length).fill(0).map(()=>new Round(randOneAniCapture()));
+		this.rounds.push(...rounds);
+	}
+	nextRound(){
+		this.nowRoundIndex++;
+		const lastRoundLength = this.rounds.length - this.nowRoundIndex;
+		if(lastRoundLength < 3){ // 剩余回合小于3个
+			this.createRounds(5);
+		}
+	}
+	step(){
+		clearTimeout(this.timer);
+		this.nowUnix = getUnix();
+
+		if(this.status === 'wait'){
+			// 等待状态
+		}
+		else if(this.status === 'playing'){
+			// 游戏中
+			const { startUnix, nowUnix, rounds } = this;
+			const leftScound = defaultScound + this.plusScound - Math.floor((nowUnix - startUnix) / 1000);
+			this.leftScound = leftScound;
+			this.progress = (nowUnix - startUnix + 1) / 1000 / (defaultScound + this.plusScound);
+
+			if(leftScound <= 0){
+				return this.toEnd();
+			}
+
+			this.timer = setTimeout(() => {
+				this.step();
+			},1000);
+		}
+	}
+	
+	toEnd(){
+		this.status = 'end';
+		this.endUnix = getUnix();
+		// 计算分数
+	}
+
+	adjestScore(value){
+		this.score += value;
+
+		if(this.score < 0){
+			this.score = 0;
+		}
+	}
+
+	// 加时
+	addScound(value){
+		this.plusScound += value;
+	}
+
+	chooseValue(value){
+		const { nowRound } = this;
+		if(!nowRound){
+			return;
+		}
+		nowRound.value = value;
+		const unix = getUnix();
+		const used = unix - nowRound.created;
+		nowRound.chooseUnix = unix;
+		nowRound.used = used;
+
+		if(value === nowRound.answer){ // 答对
+			this.adjestScore(10);
+			this.addScound(1);
+			console.log('答对');
+		}
+		else { // 答错
+			this.adjestScore(-5);
+			this.addScound(-1);
+			console.log('答错');
+		}
+
+		this.nextRound();
+	}
+}
+
+const v = new Vue({
+	el: '.app',
+	data: {
+		status: 'wating', // wating, playing, end
+		game: null,
+	},
+	computed:{
+		lastRound(){
+			return this.rounds[this.rounds.length-1];
+		}
+	},
+	methods: {
+		startGame(){
+			this.game = new Game();
+		},
+	}
+})
+
+
+const flxImage = path=>`https://image.anitabi.cn/${path}?plan=h360`;
+
+
+
+let bangumis = [];
+let Bangumis = {};
+let randNames = [];
+
+class Bangumi {
+	constructor({ name, images }){
+		this.name = name;
+		this.images = images;
+		this.randImages();
+	}
+	randImages(){
+		this.imageIndex = 0;
+		this.images = this.images.sort(_=>Math.random()-0.5);
+	}
+	getImage(){
+		this.imageIndex++;
+		if(this.imageIndex >= this.images.length){
+			this.randImages();
+		}
+		return flxImage(this.images[this.imageIndex]);
+	}
+}
+
+fetch('bangumis.json')
+	.then(r => r.json())
+	.then(_bangumis => {
+		_bangumis = _bangumis.filter(r=>!/小林/.test(r[0])).map(([name, images]) => (new Bangumi({ name, images })));
+		for(const bangumi of _bangumis){
+			Bangumis[bangumi.name] = bangumi;
+		}
+		bangumis = Object.freeze(_bangumis);
+	})
+
+
+const preloadImage = (url)=>{
+	const img = new Image();
+	img.src = url;
+	return img;
+}
+
+
+const randOneAniCapture = ()=>{
+	const names = getRandNames();
+	const answer = Math.floor(Math.random() * randNamesLength);
+	const name = names[answer];
+	const bangumi = Bangumis[name];
+	const image = bangumi.getImage();
+	const imageEl = preloadImage(image);
+	const created = getUnix();
+	return { names, answer, image, imageEl, created };
+}
+
+
+const getRandNames = ()=>{
+	if(randNames.length < randNamesLength){
+		randNames = [
+			...new Set([
+				...randNames,
+				...bangumis.map(b=>b.name).sort(_=>Math.random()-0.5)
+			])
+		];
+	}
+
+	return randNames.splice(0,randNamesLength);
+}
+
+
+
+// 快捷键功能
+document.addEventListener('keydown', e => {
+	const { key } = e;
+	const selector = `[data-key="${key}"]`;
+	const el = document.querySelector(selector);
+	if(!el) return;
+	el.setAttribute('data-hover', 'true');
+});
+document.addEventListener('keyup', e => {
+	const { key } = e;
+	const selector = `[data-key="${key}"]`;
+	const el = document.querySelector(selector);
+	if(!el) return;
+	el.removeAttribute('data-hover');
+	el.click();
+});
---
+++ 数据整理.mjs
@@ -0,0 +1,31 @@
+let bangumis = map.v.bangumis.filter(b=>/全金|凉宫|幸运|clannad|轻音|冰菓|中二病|玉子|境界的|Free|甘城|吹响|五彩线|紫罗兰|弦音|二十世纪|摇曳|小市民|安达|恋爱小|哭泣少女|你的名字|博多豚|擅长捉|我们仍|向山进|言叶|青春笨|天体的|本田小|女子落|孤独摇|放学后|夏日重|想哭的我|乒乓|莉可丽|相合之|月色真|星合之|极速车|玲音|末班列|卫宫家|干物妹|比宇宙更|只有我|空之音|夏洛特|雪之|请问您今|春宵苦短|四月是你|夜晚的水|侧耳倾听|Slow Start|道口时间|街角魔族/.test(b.cn)).sort((b,a)=>b.id - a.id)
+
+// console.log(bangumis)
+const Bangumis = {};
+
+for (let index = 0; index < bangumis.length; index++) {
+	const bangumi = bangumis[index];
+	const name2 = (bangumi.cn||bangumi.name).replace(/剧场版|电影/,'').trim().slice(0, 3);
+	// console.log(name2,bangumi)
+	if (!Bangumis[name2]) {
+		Bangumis[name2] = bangumi;
+	}else{
+		Bangumis[name2].points = Bangumis[name2].points.concat(bangumi.points);
+	}
+}
+
+
+
+bangumis = Object.values(Bangumis).sort((b,a)=>b.id - a.id);
+
+bangumis = bangumis.filter(b=>b.points.filter(p=>p.image).length).map(b=>{
+	const name = b.cn || b.name;
+	const images = b.points.filter(p=>p.image).map(p=>p.image.replace(/^\/images\//,''))
+
+	return [
+		name,
+		images,
+	]
+})
+copy(JSON.stringify(bangumis))
+console.log(bangumis)
